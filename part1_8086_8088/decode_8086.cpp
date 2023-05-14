e96df0cc2decbccecca22d486212c347afe48dab
Nicholas Lukasevich <n.luka784@gmail.com>
2023-05-14 16:32:14 -0400

    Reorganize

--- part1_8086_8088/decode_8086.cpp
+++ part1_8086_8088/decode_8086.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <filesystem>
 #include <fstream>
+#include <cstring>
+#include <cassert>
 
 namespace fs = std::filesystem;
 
@@ -20,22 +22,29 @@ int main(int argc, char const **argv) {
    std::vector<uint8_t> instruction_stream;
 
    {
-      char const *file_path = argv[1];
       [[maybe_unused]] auto const cwd = fs::current_path();
+
+      char const *file_path = argv[1];
       if (!fs::is_regular_file(file_path)) {
          std::cout << "error, not a regular file\n";
          std::exit(1);
       }
+
       size_t const file_size = static_cast<size_t>(fs::file_size(file_path));
+      assert(file_size <= (size_t)std::numeric_limits<std::streamsize>::max());
+
       instruction_stream.resize(file_size);
+
       std::ifstream file(file_path, std::ios::binary);
       CRASH_IF(!file);
+
       try {
-         file.read(reinterpret_cast<char *>(instruction_stream.data()), file_size);
+         file.read(reinterpret_cast<char *>(instruction_stream.data()), std::streamsize(file_size));
       } catch (std::exception const &except) {
          std::cout << "error reading file, " << except.what() << '\n';
          std::exit(1);
       }
+
       CRASH_IF(static_cast<size_t>(file.gcount()) != file_size);
    }
 
@@ -46,12 +55,12 @@ int main(int argc, char const **argv) {
       uint8_t const byte1 = instruction_stream[i];
       uint8_t const byte2 = instruction_stream[i + 1];
 
-      bool const goodOpcode = (byte1 >> 2) & 0b100010;
-      bool const destComesFirst = (byte1 & 0b10);
-      bool const wide = (byte1 & 0b1);
-      bool const registerToRegister = (byte2 >> 6) & 0b11;
+      bool const good_opcode = (byte1 >> 2) & 0b100010;
+      bool const dest_comes_first = (byte1 & 0b10);
+      bool const is_wide = (byte1 & 0b1);
+      [[maybe_unused]] bool const reg_to_reg = (byte2 >> 6) & 0b11;
 
-      if (!goodOpcode) {
+      if (!good_opcode) {
          std::cout << "error, bad opcode for instruction " << i << '\n';
          std::exit(1);
       }
@@ -67,7 +76,7 @@ int main(int argc, char const **argv) {
             else if ((code & 0b010) == 0b010) reg = "dx";
             else if ((code & 0b001) == 0b001) reg = "cx";
             else    /* 0b000 */               reg = "ax";
-            memcpy(out, reg, 2);
+            std::memcpy(out, reg, 2);
          } else {
             if      ((code & 0b11) == 0b11) out[0] = 'b';
             else if ((code & 0b10) == 0b10) out[0] = 'd';
@@ -80,13 +89,13 @@ int main(int argc, char const **argv) {
          }
       };
 
-      uint8_t const dst_register_code = (destComesFirst ? (byte2 >> 3) : byte2) & 0b00000111;
-      uint8_t const src_register_code = (destComesFirst ? (byte2) : (byte2 >> 3)) & 0b00000111;
+      uint8_t const dst_register_code = (dest_comes_first ? (byte2 >> 3) : byte2) & 0b00000111;
+      uint8_t const src_register_code = (dest_comes_first ? (byte2) : (byte2 >> 3)) & 0b00000111;
 
       char dst_register_str[3] { '\0' };
       char src_register_str[3] { '\0' };
-      decode_register(wide, dst_register_code, dst_register_str);
-      decode_register(wide, src_register_code, src_register_str);
+      decode_register(is_wide, dst_register_code, dst_register_str);
+      decode_register(is_wide, src_register_code, src_register_str);
 
       std::cout << "mov " << dst_register_str << ", " << src_register_str << '\n';
    }
